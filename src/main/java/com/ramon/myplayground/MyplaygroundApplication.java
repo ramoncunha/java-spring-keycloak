045431844165255c27ba6ca1c9b267cafe179870
Ramon Cunha <ramon.network@hotmail.com>
2023-06-08 15:21:59 -0300

    feat(project): Initialize project with basic structure and configuration files

---
+++ src/main/java/com/ramon/myplayground/MyplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.myplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplaygroundApplication.class, args);
+	}
+
+}
