166b465eb3d89d4120d527b77f18cb9905411498
Ramon Cunha <ramon.network@hotmail.com>
2023-06-14 21:22:36 -0300

    feat: add swagger, adjusted exception

--- src/main/java/com/ramon/myplayground/domain/exceptions/CarNotFoundException.java
+++ src/main/java/com/ramon/myplayground/domain/exceptions/CarNotFoundException.java
@@ -1,5 +1,9 @@
 package com.ramon.myplayground.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CarNotFoundException extends RuntimeException {
 
     public CarNotFoundException() {
