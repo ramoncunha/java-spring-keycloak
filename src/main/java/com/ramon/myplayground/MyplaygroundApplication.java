4f11a5047b9f0f19ba73c58dc7c52ea07f534f52
Ramon Cunha <ramon.network@hotmail.com>
2023-08-09 22:29:10 -0300

    refactor: changed development configs, create dependency on docker-compose

--- src/main/java/com/ramon/myplayground/MyplaygroundApplication.java
+++ src/main/java/com/ramon/myplayground/MyplaygroundApplication.java
@@ -2,13 +2,11 @@ package com.ramon.myplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class MyplaygroundApplication {
 
 	public static void main(String[] args) {
-		System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
 		SpringApplication.run(MyplaygroundApplication.class, args);
 	}
 
