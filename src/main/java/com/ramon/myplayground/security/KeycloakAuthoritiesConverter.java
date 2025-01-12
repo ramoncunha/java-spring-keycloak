63c8fd60e172f144fafc00361049cd44ac34cd96
Ramon Cunha <ramon.network@hotmail.com>
2025-01-12 15:38:42 -0300

    fix jwt roles mapping

--- src/main/java/com/ramon/myplayground/security/KeycloakAuthoritiesConverter.java
+++ src/main/java/com/ramon/myplayground/security/KeycloakAuthoritiesConverter.java
@@ -1,6 +1,5 @@
 package com.ramon.myplayground.security;
 
-import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.GrantedAuthority;
@@ -8,8 +7,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.function.Function;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,46 +18,22 @@ import java.util.stream.Stream;
 public class KeycloakAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     public static final String ROLE_PREFIX = "ROLE_";
-    private static final String ROLE_LOCALTION = "resource_access";
+    private static final String RESOURCE_ACCESS = "resource_access";
     private static final String CLIENT_NAME = "car-api";
     private static final String ROLES = "roles";
 
     @Override
     public Collection<GrantedAuthority> convert(@NonNull final Jwt jwt) {
-        final var realmRoles = extractRealmRoles(jwt);
-        final var resourceRoles = extractResourceRoles(jwt);
-        return Stream.concat(realmRoles, resourceRoles)
+        return extractRealmRoles(jwt)
                 .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role.toUpperCase()))
                 .collect(Collectors.toSet());
     }
 
     @SuppressWarnings("unchecked")
     private Stream<String> extractRealmRoles(final Jwt jwt) {
-        return Optional.ofNullable(jwt.getClaimAsMap(ROLE_LOCALTION))
+        return Optional.ofNullable(jwt.getClaimAsMap(RESOURCE_ACCESS))
                 .map(resource -> (Map<String, Collection<String>>) resource.get(CLIENT_NAME))
                 .map(resource -> resource.get(ROLES))
-                .orElse(Collections.emptyList())
-                .stream();
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private Stream<String> extractResourceRoles(final Jwt jwt) {
-        final Function<Map.Entry<String, Object>, Stream<String>> mapResources =
-                resource -> {
-                    final var key = resource.getKey();
-                    final var value = (LinkedTreeMap) resource.getValue();
-                    final var roles = (Collection<String>) value.get(ROLES);
-                    return roles.stream().map(role -> key.concat("_").concat(role));
-                };
-
-        final Function<Set<Map.Entry<String, Object>>, Collection<String>> mapResource =
-                resources -> resources.stream()
-                        .flatMap(mapResources)
-                        .toList();
-
-        return Optional.ofNullable(jwt.getClaimAsMap("resource_access"))
-                .map(Map::entrySet)
-                .map(mapResource)
                 .orElse(Collections.emptyList())
                 .stream();
     }
